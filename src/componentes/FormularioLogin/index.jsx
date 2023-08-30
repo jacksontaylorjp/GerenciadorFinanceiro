16929a4120aba62906382d1e103e5753b7909e31
jacksontaylorjp <jacksontaylorjp@gmail.com>
2023-08-30 12:46:17 -0300

    alteração da tela de login para componentes do material ui

--- src/componentes/FormularioLogin/index.jsx
+++ src/componentes/FormularioLogin/index.jsx
@@ -1,17 +1,64 @@
-import "./FormularioLogin.css"
-import CampoTexto from "../CampoTexto";
-import Botao from "../Botao";
+import { Button, TextField } from "@mui/material";
+import { useState } from "react";
+
 const FormularioLogin = () => {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const [erros, setErros] = useState({email:{valido:true,texto:""}});
+    
+    //tratando o erro do email
+    function erroEmail(){
+        if(email.length < 3 ){
+            setErros({email:{valido:false, texto: "O email dever ter mais de 3 caracteres"}});
+        }else{
+            setErros({email:{valido:true, texto: ""}});
+        }
+    }
+
     return(
-        <div className="containerFormLogin">
-            <img src="https://cdn-icons-png.flaticon.com/128/871/871589.png" alt="Logo"/>
-            <h1>Login</h1>
-            <form>
-                <CampoTexto label="Email"/>
-                <CampoTexto label="Senha"/>
-                <Botao label="Entrar"/>
-            </form>          
-        </div>
+        <form onSubmit={event => {
+            event.preventDefault();
+            console.log(email);
+            console.log(senha);
+        }}
+        >
+            <TextField 
+                        id="email" 
+                        label="Email" 
+                        variant="outlined"
+                        margin="normal"
+                        type="email"
+                        fullWidth
+                        value={email}
+                        onChange={(event) => {
+                            setEmail(event.target.value);
+                        }}
+
+                        error={!erros.email.valido}
+                        helperText={erros.email.texto}
+                        onBlur={(event) => {erroEmail();}}
+                    />
+            <TextField 
+                        id="senha" 
+                        label="Senha" 
+                        variant="outlined"
+                        margin="normal"
+                        type="password"
+                        fullWidth 
+                        value={senha}
+                        onChange={(event) => {
+                            setSenha(event.target.value);
+                        }}
+                    />
+            <Button
+                fullWidth 
+                variant="contained"
+                type="submit"
+            >
+                Entrar
+            </Button>
+        </form>
     );
 }
 
--- src/componentes/PagLogin/index.jsx
+++ src/componentes/PagLogin/index.jsx
@@ -1,10 +1,40 @@
-import "./PagLogin.css";
+import {Container, Grid, Typography, Link } from "@mui/material";
+import '@fontsource/roboto/400.css';
 import FormularioLogin from "../FormularioLogin";
 const PagLogin = () => {
     return(
-        <div className="containerPagLogin">
-            <FormularioLogin />
-        </div>
+        <Container 
+            maxWidth="xm"
+            sx={{
+                display: "flex",
+                alignItems: "center",
+                height: "100vh",
+                typography: 'body1',
+            }}
+        >
+            <Grid
+                container
+                direction="column"
+                justifyContent="space-around"
+                alignItems="center"
+            >
+                <img src="https://cdn-icons-png.flaticon.com/128/871/871589.png"></img>
+                <Typography 
+                    variant="h4"
+                    component="h1"
+                >
+                    Faça seu login
+                </Typography>
+                <FormularioLogin />
+                <Link 
+                    href="#"
+                    marginTop={1}
+                >
+                    {'Esqueceu a sua senha?'}
+                </Link>
+            </Grid>
+        </Container>
+
     );
 }
 
