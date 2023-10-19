384ee88d30ac635298f73e0bf0e3b6dc93026c3a
jacksontaylorjp <jacksontaylorjp@gmail.com>
2023-10-18 22:23:49 -0300

    feat: page inicial

--- src/pages/PagInicial/index.js
+++ src/pages/PagInicial/index.js
@@ -1,12 +1,31 @@
+import style from './PagInicial.module.css'
+import { Button, Col, Row, Typography } from "antd";
+
 const PagInicial = () => {
     function login() {
         window.location.href = "http://localhost:3000/login";
     }
+    const { Title } = Typography;
     return (
-        <>
-            <h1>Página Inicial</h1>
-            <button onClick={() => login()}>Login</button>
-        </>
+        <div className={style.divPagInicial}>
+            <Row justify="center">
+                <Col>
+                    <Title level={2}>Bem-vindo</Title>
+                </Col>
+            </Row>
+            <Row justify="center" gutter={48}>
+                <Col>
+                    <Button type="primary" shape="round" size='large' onClick={() => login()}>
+                        Entrar
+                    </Button>
+                </Col>
+                <Col>
+                    <Button type="primary" shape="round" size='large' disabled>
+                        Cadastre-se
+                    </Button>
+                </Col>
+            </Row>
+        </div>
     );
 };
 
